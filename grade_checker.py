c9c3fb926381a40147e0bb5750b180777c18b325
shrutich-30 <shrutichaurasiya201@gmail.com>
2026-06-11 22:47:39 +0530

    added student grade checker CI pipeline

---
+++ grade_checker.py
@@ -0,0 +1,28 @@
+def calculate_grade(marks):
+
+    if marks < 0 or marks > 100:
+        raise ValueError("Marks should be between 0 and 100")
+
+    if marks >= 90:
+        return "A"
+
+    elif marks >= 75:
+        return "B"
+
+    elif marks >= 60:
+        return "C"
+
+    elif marks >= 40:
+        return "D"
+
+    else:
+        return "F"
+
+
+def student_result(name, marks):
+
+    return {
+        "name": name,
+        "marks": marks,
+        "grade": calculate_grade(marks)
+    }
